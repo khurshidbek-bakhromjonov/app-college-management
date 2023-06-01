9f0e03ea793ebadc5aaa889463f28914f83aa5f5
Khurshidbek Bakhromjonov <khurshidbekjava@gmail.com>
2023-06-01 09:27:44 +0500

    first touch

---
+++ src/main/java/org/college/AppCollegeManagementApplication.java
@@ -0,0 +1,13 @@
+package org.college;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCollegeManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppCollegeManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/college/AppCollegeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.college;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppCollegeManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
